a663aa93f1d07cbf4144f0a75145d51bfbbba0e9
nibushibu <nibushibu@gmail.com>
2013-06-06 10:53:57 +0900

    config.rb修正

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ javascripts_dir = "js"
 output_style = (environment == :production) ? :compressed : :expanded
 line_comments = (environment == :production) ? :false : :true
 relative_assets = true
-sass_options = { :debug_info => false }
+sass_options = (environment == :production) ? { :debug_info => false } : { :debug_info => true }
 
 # # Make a copy of sprites with a name that has no uniqueness of the hash.
 # on_sprite_saved do |filename|
