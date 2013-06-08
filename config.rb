8d01b3883dfd11ffd94d5ab00110543c0bae8289
nibushibu <nibushibu@gmail.com>
2013-06-09 02:04:19 +0900

    config.rb修正

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 output_style = (environment == :production) ? :compressed : :expanded
-#line_comments = (environment == :production) ? :false : :true
+#line_comments = (environment == :production) ? false : true
 line_comments = false
 relative_assets = true
 sass_options = (environment == :production) ? { :debug_info => false } : { :debug_info => true }
