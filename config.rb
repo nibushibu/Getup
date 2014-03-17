8174f50d6f8d631a9edaf226f8339b4ac3310f6a
Genki Katsutani <nibushibu@gmail.com>
2014-03-17 18:34:29 +0900

    compass-animationを削除

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ output_style = (environment == :production) ? :compressed : :expanded
 line_comments = (environment == :production) ? false : true
 # forcecompile = (environment == :production) ? true : false
 relative_assets = true
-require 'animation'
+# require 'animation'
 # sass_options = (environment == :production) ? { :sourcemap => false} : { :sourcemap => true }
 # sass_options = (environment == :production) ? { :debug_info => false, :sourcemap => false} : { :debug_info => true, :sourcemap => true }
 # line_comments = false
