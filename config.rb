dfa6d8b982d39b7a2a268ce50511954f3ca082f7
Genki Katsutani <nibushibu@gmail.com>
2014-05-09 02:23:26 +0900

    Compassのeasingプラグインceaser-easingを導入

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ output_style = (environment == :production) ? :compressed : :expanded
 line_comments = (environment == :production) ? false : true
 # forcecompile = (environment == :production) ? true : false
 relative_assets = true
+require 'ceaser-easing'
 # require 'animation'
 # sass_options = (environment == :production) ? { :sourcemap => false} : { :sourcemap => true }
 # sass_options = (environment == :production) ? { :debug_info => false, :sourcemap => false} : { :debug_info => true, :sourcemap => true }
