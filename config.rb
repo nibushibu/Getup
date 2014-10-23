506fda1a8be0783561ddf1ba0814ffe195e3704d
Genki Katsutani <nibushibu@gmail.com>
2014-10-23 14:01:29 +0900

    修正

--- config.rb
+++ config.rb
@@ -5,12 +5,12 @@ images_dir = "public/img"
 javascripts_dir = "public/js"
 output_style = (environment == :production) ? :compressed : :expanded
 line_comments = (environment == :production) ? false : true
-# forcecompile = (environment == :production) ? true : false
 relative_assets = true
 require 'ceaser-easing'
 # require 'animation'
 # sass_options = (environment == :production) ? { :sourcemap => false} : { :sourcemap => true }
 # sass_options = (environment == :production) ? { :debug_info => false, :sourcemap => false} : { :debug_info => true, :sourcemap => true }
+# forcecompile = (environment == :production) ? true : false
 # line_comments = false
 
 # # Make a copy of sprites with a name that has no uniqueness of the hash.
