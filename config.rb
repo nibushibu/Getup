8619045158bd7ab48e5cf3c56246c0c76e610f80
Genki Katsutani <nibushibu@gmail.com>
2015-06-26 23:45:59 +0900

    publicをappに修正

--- config.rb
+++ config.rb
@@ -1,8 +1,8 @@
-http_path = "public/"
-css_dir = "public/css"
+http_path = "app/"
+css_dir = http_path + "css"
 sass_dir = "scss"
-images_dir = "public/img"
-javascripts_dir = "public/js"
+images_dir = http_path + "img"
+javascripts_dir = http_path + "js"
 output_style = (environment == :production) ? :compressed : :expanded
 line_comments = (environment == :production) ? false : true
 relative_assets = true
