ead81a7284c865697e75656283afbfee7c84539b
Genki Katsutani <nibushibu@gmail.com>
2015-10-18 11:00:40 +0900

    更新

--- app/js/plugins-base.js
+++ app/js/plugins-base.js
@@ -1,22 +1,2 @@
 // Avoid `console` errors in browsers that lack a console.
-'use strict';
-
-(function () {
-  var method;
-  var noop = function noop() {};
-  var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
-  var length = methods.length;
-  var console = window.console = window.console || {};
-
-  while (length--) {
-    method = methods[length];
-
-    // Only stub undefined methods.
-    if (!console[method]) {
-      console[method] = noop;
-    }
-  }
-})();
-
-// Place any jQuery/helper plugins in here.
-//# sourceMappingURL=plugins-base.js.map
+"use strict";!function(){for(var method,noop=function(){},methods=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"],length=methods.length,console=window.console=window.console||{};length--;)method=methods[length],console[method]||(console[method]=noop)}();
