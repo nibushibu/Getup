753d839b79ebed0d056bf9e45e1ecf10c54ae034
Genki Katsutani <nibushibu@gmail.com>
2015-11-12 07:10:11 +0900

    更新

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,1 @@
-// Easingの追加
 "use strict";jQuery.easing.quart=function(x,t,b,c,d){return-c*((t=t/d-1)*t*t*t-1)+b},$(function(){$("a[href*=#]").click(function(){var $target,targetOffset;return location.pathname.replace(/^\//,"")===(void 0).pathname.replace(/^\//,"")&&location.hostname===(void 0).hostname&&($target=$((void 0).hash),$target=$target.length&&$target||$("[name="+(void 0).hash.slice(1)+"]"),$target.length)?(targetOffset=$target.offset().top,$("html,body").animate({scrollTop:targetOffset},200,"quart"),!1):void 0})});
