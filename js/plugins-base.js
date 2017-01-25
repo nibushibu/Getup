9a105a7395879628712aaf7407ab02934225093b
Genki Katsutani <nibushibu@gmail.com>
2017-01-25 23:48:58 +0900

    Update plugin-base.js
    プラグインをまとめる雛形JSを見直し。
    html5 boilerplateを参照。
    自作の高さFixメソッドは使ってないので削除。

--- js/plugins-base.js
+++ js/plugins-base.js
@@ -1,12 +1,12 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
   var method;
-  var noop = function () {};
+  var noop = function() {};
   var methods = [
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
     'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeStamp', 'trace', 'warn'
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
   ];
   var length = methods.length;
   var console = (window.console = window.console || {});
@@ -22,62 +22,3 @@
 }());
 
 // Place any jQuery/helper plugins in here.
-
-/*
- * resizeHeight
- *
- * Author Genki Katsutani
- * http://nibushibu.com/
- *
- * Licensed under the MIT License
- */
-
-(function($){
-
-  function resizeHeight(){
-
-    var el = $('.js-resize-height');
-    var timer = false;
-
-    $(window).resize(function() {
-
-      el.height('auto');
-
-      if (timer !== false) {
-        clearTimeout(timer);
-      }
-
-      timer = setTimeout(function() {
-        resize();
-      }, 200);
-    });
-
-    function resize(){
-
-      var obj = [];
-      var keyArr = [];
-
-      for (var i = el.length - 1; i >= 0; i--) {
-        var key = $('.js-resize-height:eq(' + i + ')').data('resize-group');
-        var val = $('.js-resize-height:eq(' + i + ')').height();
-        if(!obj[key]){
-          obj[key] = [];
-          keyArr.push(key);
-        }
-        obj[key].push(val);
-      };
-      for (var i = keyArr.length - 1; i >= 0; i--) {
-        var maxHeight = Math.max.apply(null, obj[keyArr[i]]);
-        // console.log(maxHeight);
-        $('.js-resize-height[data-resize-group="' + keyArr[i] + '"]').height(maxHeight);
-      };
-    }
-
-    resize();
-  }
-
-  $(function(){
-    resizeHeight();
-  })
-
-})(jQuery);
