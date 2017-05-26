21a0cd9956d2b45cfc44616a4169461d498b9852
Genki Katsutani <nibushibu@gmail.com>
2017-05-26 17:42:44 +0900

    Update min task

--- gulpfile.babel.js/tasks/min.js
+++ gulpfile.babel.js/tasks/min.js
@@ -4,17 +4,6 @@ let $ = require('gulp-load-plugins')();
 let gutil = require('gulp-util');
 let runSequence = require('run-sequence');
 let licenseRegexp = /^\!|^@preserve|^@cc_on|\bMIT\b|\bMPL\b|\bGPL\b|\(c\)|License|Copyright/i;
-let isLicenseComment = (function () {
-  let _prevCommentLine = 0;
-  return (node, comment) => {
-    if (licenseRegexp.test(comment.value) || comment.line === 1 || comment.line === _prevCommentLine + 1) {
-      _prevCommentLine = comment.line;
-      return true;
-    }
-    _prevCommentLine = 0;
-    return false;
-  };
-})();
 
 gulp.task('min', callback => {
   return runSequence(
