23130789ea7d8cc9cd33a8e90b534ef45f141951
Genki Katsutani <nibushibu@gmail.com>
2017-08-09 19:35:42 +0900

    Update
    Remove @nest in css
    Update index.html template
    etc.

--- gulpfile.babel.js/tasks/watch.js
+++ gulpfile.babel.js/tasks/watch.js
@@ -8,7 +8,7 @@ gulp.task('watch', () => {
   gulp.watch('svg/*.svg', ['icons']);
   gulp.watch('iconfont/*.svg', ['iconfont']);
   gulp.watch(['css/**/*.css', 'riot/**/*.css'], ['css']);
-  gulp.watch('js/**/*.js', ['webpack']);
+  gulp.watch('js/**/*.js', ['babel']);
   gulp.watch('ejs/**/*.ejs', ['ejs']);
   gulp.watch('sprite/**/*', ['sprite']);
   gulp.watch('riot/**/*.+(tag|tag.html)', ['riot']);
