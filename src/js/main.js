4ac2e27009f2aecf0dae49d271b64513c3094744
Genki Katsutani <nibushibu@gmail.com>
2017-08-20 16:14:35 +0900

    Update
    もろもろ調整。
    mo-jsは何故かエラーが出るのでanimejsに

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,24 @@
-import slick from 'slick-carousel'
-import mojs from 'mo-js'
+import jQuery from "jquery"
+window.$ = window.jQuery = jQuery
+
+import 'whatwg-fetch'
+import Promise from 'promise-polyfill'
 import riot from 'riot'
+import anime from 'animejs'
+import ScrollMagic from 'scrollmagic'
+import slick from 'slick-carousel'
+import MobileDetect from 'mobile-detect'
+
 import './app.tag'
 
-riot.mount(app)
+// riot.mount('app')
 
 (function($){
 
 $(() => {
 
   // スマホ判定
-  var _ua = (function(u){
-    return {
-      Tablet:(u.indexOf('windows') != -1 && u.indexOf('touch') != -1)
-        || u.indexOf('ipad') != -1
-        || (u.indexOf('android') != -1 && u.indexOf('mobile') == -1)
-        || (u.indexOf('firefox') != -1 && u.indexOf('tablet') != -1)
-        || u.indexOf('kindle') != -1
-        || u.indexOf('silk') != -1
-        || u.indexOf('playbook') != -1,
-      Mobile:(u.indexOf('windows') != -1 && u.indexOf('phone') != -1)
-        || u.indexOf('iphone') != -1
-        || u.indexOf('ipod') != -1
-        || (u.indexOf('android') != -1 && u.indexOf('mobile') != -1)
-        || (u.indexOf('firefox') != -1 && u.indexOf('mobile') != -1)
-        || u.indexOf('blackberry') != -1
-    }
-  })(window.navigator.userAgent.toLowerCase());
+  const md = new MobileDetect(window.navigator.userAgent);
 
   // アンカースクロールアニメーション
   const anchorScroll = () => {
@@ -48,7 +40,7 @@ $(() => {
       });
     }
 
-    if(!_ua.Tablet){
+    if(!md.mobile()){
       $('a[href^="#"], .js-anchor-scroll').on('click', (e) => {
         e.preventDefault();
         var href = $(e.currentTarget).attr('href');
