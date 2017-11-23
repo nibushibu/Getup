1c59cb7a42ec58c100e868b4967414fd351692e5
Genki Katsutani <nibushibu@gmail.com>
2017-11-23 16:07:48 +0900

    Add css custom parser  for riot tag

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,33 @@ var pluginsArray = process.env.NODE_ENV === 'production'
   ? productionPluginsArray
   : developPluginsArray
 
+/**
+ * Riotのカスタムパーサーの定義に必要なモジュールの読み込み
+ */
+const riot = require('riot')
+const sass = require('node-sass')
+const postcss = require('postcss')
+const postcssFocus = require('postcss-focus')
+const cssMqpacker = require('css-mqpacker')
+const postcssFlexibility = require('postcss-flexibility')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+
+/**
+ * Riot用CSSカスタムパーサー（Sass + Autoprefixer）
+ */
+riot.parsers.css.myCssParser = function (tagName, css) {
+
+  const sassOptions = {
+    data: css,
+    indentType: 'space'
+  }
+
+  css = sass.renderSync(sassOptions).css
+  css = postcss([autoprefixer]).process(css).css
+  return css
+}
+
 module.exports = {
 
   // エントリーファイル
