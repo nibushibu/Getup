fccbcf9068f3ff576e67983648c03365a8ed8a2c
Genki Katsutani <nibushibu@gmail.com>
2018-04-25 19:10:09 +0900

    Delete iconfont

--- gulpfile.babel.js/config.js
+++ gulpfile.babel.js/config.js
@@ -2,10 +2,6 @@ module.exports = {
   /* 特別な定義が不要なタスクの設定 */
   appPath: 'dist/',
 
-  iconfont: {
-    sketch: 'src/symbol-font-14px.sketch'
-  },
-
   js: [
     'node_modules/riot/riot.min.js',
     'node_modules/jquery/dist/jquery.min.js',
