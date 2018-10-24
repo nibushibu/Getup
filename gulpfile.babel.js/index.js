a89bc3255a0a700749eb2cba03eff30e80e5d398
Genki Katsutani <nibushibu@gmail.com>
2018-10-24 10:13:29 +0900

    Remove gulp

--- gulpfile.babel.js/index.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var requireDir = require('require-dir');
-requireDir('./tasks', { recurse: true });
-var $ = require('gulp-load-plugins')();
-var runSequence = require('run-sequence');
-
-gulp.task('default', function (callback) {
-  return runSequence('watch', callback);
-});
