fda043f130b5f09bc08e6fef0072795fee1c41a1
Genki Katsutani <nibushibu@gmail.com>
2019-03-28 22:40:18 +0900

    buble削除

--- dist/js/tags.js
+++ dist/js/tags.js
@@ -9,40 +9,42 @@
   }
 })(function(riot, require, exports, module) {
 riot.tag2('app-tag', '<div> <h1>Thie is Riot test.</h1> <button class="unmount-animation" onclick="{animationUnmount}" ref="button">アンマウント！</button> </div>', 'app-tag h1,[data-is="app-tag"] h1{ color: orange; display: flex; } app-tag .unmount-animation,[data-is="app-tag"] .unmount-animation{ opacity: 1; transition: opacity 1s; } app-tag .unmount-animation.is-unmount,[data-is="app-tag"] .unmount-animation.is-unmount{ opacity: 0; }', '', function(opts) {
-var tag = this;
-tag.finishAnimation = false;
-tag.on('mount', function (e) {
-  var tl = anime.timeline({
-    duration: 500
-  });
-  tl.add({
-    targets: tag.refs.button,
-    opacity: 0,
-    duration: 1
-  }).add({
-    targets: tag.refs.button,
-    opacity: 1,
-    duration: 1000
-  });
-});
-tag.test = 'Hello Riot!';
+    const tag = this
+    tag.finishAnimation = false
 
-tag.animationUnmount = function (e) {
-  tag.refs.button.addEventListener('transitionend', function (e) {
-    console.log('きえた');
-    tag.unmount();
-  });
-  tag.refs.button.classList.add('is-unmount');
-};
+    tag.on('mount', e => {
+      var tl = anime.timeline({
+        duration: 500
+      })
+      tl.add({
+        targets: tag.refs.button,
+        opacity: 0,
+        duration: 1
+      })
+      .add({
+        targets: tag.refs.button,
+        opacity: 1,
+        duration: 1000
+      })
+    })
 
-tag.on('before-unmount', function (e) {
-  console.log(tag);
-});
+    tag.test = 'Hello Riot!'
+
+    tag.animationUnmount = e => {
+      tag.refs.button.addEventListener('transitionend', e => {
+        console.log('きえた')
+        tag.unmount()
+      })
+      tag.refs.button.classList.add('is-unmount')
+    }
+
+    tag.on('before-unmount', e => {
+      console.log(tag)
+    })
 });
 riot.tag2('raw-html', '<span></span>', '', '', function(opts) {
-this.root.innerHTML = opts.content;
-this.on('update', function () {
-  this.root.innerHTML = opts.content;
-});
+    this.root.innerHTML = opts.content
+    this.on('update', function() {
+      this.root.innerHTML = opts.content
+    })
 });});
-//# sourceMappingURL=tags.js.map
