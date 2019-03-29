e6faea70f15ff741fd2d20f7421acce412bbf109
Genki Katsutani <nibushibu@gmail.com>
2019-03-29 09:53:56 +0900

    更新

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,14 @@
 riot.mount('*');
 (function ($) {
-  $(() => {
-    if (!feature.touch) {
-      $('a[href^="#"], .js-anchor-scroll').on('click', e => {
-        e.preventDefault()
-        let headerHeight = 0
-        let $target = $(e.currentTarget.attributes.href.value)
-        console.log($target)
-        let targetOffset = $target.offset().top
-        $('html,body').animate({
-            scrollTop: targetOffset - headerHeight - 10
-          },
-          300
-        )
-      })
-    }
+  $(function () {
+    // docmentReady後の動作
   })
 })(jQuery)
 
 console.log(`feature.touch = ${feature.touch}`)
 
 // anime.js サンプル
-const animeSample = anime({
+var animeSample = anime({
   targets: '.js-anime',
   translateX: 240,
   rotate: 360,
