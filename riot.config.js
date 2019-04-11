eb7bc1a9aca8aaf1c453115edd28dfa2e32fb8de
Genki Katsutani <nibushibu@gmail.com>
2019-04-11 09:35:51 +0900

    cliそのまま使ってみたがだめな様子

--- riot.config.js
+++ riot.config.js
@@ -7,29 +7,9 @@ const postcss = require('postcss')
 const postcssFocus = require('postcss-focus')
 const autoprefixer = require('autoprefixer')
 
-/**
- * Riot用CSSカスタムパーサー（Sass + Autoprefixer）
- */
-riot.parsers.css.myCssParser = function (tagName, css) {
-
-  const sassOptions = {
-    data: css,
-    indentType: 'space'
-  }
-
-  css = sass.renderSync(sassOptions).css
-  css = postcss([
-    postcssFocus,
-    autoprefixer({
-      grid: true
-    }),
-  ]).process(css).css
-  return css
-}
-
 export default {
   from: 'src/riot',
   to: 'dist/js/tags.js',
   ext: 'html',
-  style: 'myCssParser'
+  style: 'scss'
 }
