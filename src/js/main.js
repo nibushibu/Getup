47a258fe2f924b627d27f2f44700403dd3ba215d
Genki Katsutani <nibushibu@gmail.com>
2019-05-10 01:44:15 +0900

    Riotタグはマウントできてないけど進捗

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,7 @@
-import riot from '../../node_modules/riot/riot.esm.js'
 import App from '../riot/app.riot'
 import RawHtml from '../riot/raw-html.riot'
 
 console.log(App)
 console.log(RawHtml)
 
-riot.register('app', App)
-riot.register('raw-html', RawHtml)
-
-riot.mount('[data-riot]')
-
-// anime.js サンプル
-var animeSample = anime({
-  targets: '.js-anime',
-  translateX: 240,
-  duration: 800,
-  direction: 'alternate',
-  easing: 'easeInOutExpo',
-  loop: true
-})
+export default App
