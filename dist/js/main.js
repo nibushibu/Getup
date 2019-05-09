e8649d073a9a498d2333f29a3a088c6c26b7212c
Genki Katsutani <nibushibu@gmail.com>
2019-05-09 09:08:57 +0900

    試行錯誤

--- dist/js/main.js
+++
@@ -1,1 +0,0 @@
-import riot from"../../node_modules/riot/riot.esm.js";import App from"../riot/app.riot";import RawHtml from"../riot/raw-html.riot";console.log(App);console.log(RawHtml);riot.register("app",App);riot.register("raw-html",RawHtml);riot.mount("[data-riot]");var animeSample=anime({targets:".js-anime",translateX:240,duration:800,direction:"alternate",easing:"easeInOutExpo",loop:true});
