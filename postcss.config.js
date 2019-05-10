351e8b51f3928d192eb75383e6e799764b5310e7
Genki Katsutani <nibushibu@gmail.com>
2019-05-10 11:11:37 +0900

    Add postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-focus'),
+    require('postcss-normalize-charset'),
+    require('postcss-flexbugs-fixes'),
+    require('autoprefixer')({
+      grid: true
+    })
+  ]
+}
