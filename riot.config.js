7ccf4e1b3bd06444a2016699468793ea20de0ed8
Genki Katsutani <nibushibu@gmail.com>
2019-05-19 06:13:55 +0900

    WIP

--- riot.config.js
+++ riot.config.js
@@ -1,3 +1,21 @@
+var registerPreprocessor = require('@riotjs/compiler').registerPreprocessor
+var sass = require('node-sass')
+
+registerPreprocessor('css', 'sass', function(code, { options }) {
+  const { file } = options
+
+  console.log('Compile the sass code in', file)
+
+  const css = sass.renderSync({
+    data: code
+  })
+
+  return {
+    code: css,
+    map: null
+  }
+})
+
 module.exports = {
   css: 'sass'
 }
