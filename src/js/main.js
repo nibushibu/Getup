2429aa3c18c8278f058e39ffc1796e8d87a56915
Genki Katsutani <nibushibu@gmail.com>
2019-10-02 08:18:22 +0900

    .riot内でのscssの処理を修正

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { register, mount } from 'riot'
+import { registerPreprocessor } from '@riotjs/compiler'
 import App from '../riot/my-app.riot'
 import RawHtml from '../riot/raw-html.riot'
 
 register('my-app', App)
+
 mount('#my-app')
