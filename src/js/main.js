62fc98ba3060a11f9d8f4cd192a18ed7608020b4
Genki Katsutani <nibushibu@gmail.com>
2019-12-07 13:16:50 +0900

    Riot Routerを追加
    でもIE11で動かない

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import { register, mount, install } from 'riot'
-import { route, router } from '@riotjs/route'
+import { Route, Router } from '@riotjs/route'
 import App from '../riot/my-app.riot'
 
 register('my-app', App)
+register('app-route', Route)
+register('app-router', Router)
 
 mount('#my-app')
