eb258048d4cf36f7bd8d916124fc3e473c83fb10
Genki Katsutani <nibushibu@gmail.com>
2020-01-16 05:14:25 +0900

    いろいろ試してみたがダメだった

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import 'core-js'
 import '@webcomponents/template'
-import { register, mount, install } from 'riot'
+import * as riot from 'riot'
 import App from '../riot/my-app.riot'
+import SmileFace from '../riot/smile-face.riot'
+import RawHtml from '../riot/raw-html.riot'
 
-register('my-app', App)
+riot.register('my-app', App)
+riot.register('smile-face', SmileFace)
+riot.register('raw-html', RawHtml)
 
-mount('#my-app')
+riot.mount('#my-app')
