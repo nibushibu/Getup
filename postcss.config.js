c9bbdfd17072e1c70c6561db27070efd66287146
Genki Katsutani <nibushibu@gmail.com>
2020-05-27 09:42:56 +0900

    css var 変換とか cssnano 追加とか

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,22 @@
-module.exports = {
+module.exports = (ctx) => ({
+  map: ctx.options.map,
   plugins: [
     require('postcss-import'),
     require('postcss-preset-env')({
+      stage: 0,
       autoprefixer: { grid: true }
     }),
     require('postcss-focus'),
     require('postcss-normalize-charset'),
     require('postcss-flexbugs-fixes'),
+    require('postcss-custom-properties'),
     require('mdcss')({
       examples: {
         css: ['../dist/css/main.css']
       }
     }),
+    require('cssnano')({
+      preset: 'default',
+    }),
   ]
-}
+})
