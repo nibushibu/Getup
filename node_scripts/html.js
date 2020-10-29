cdd34fb2a19430d0cabd66384f1484b82883ab27
Genki Katsutani <nibushibu@gmail.com>
2020-10-29 09:39:25 +0900

    ディレクトリの中を見てHTML生成

--- node_scripts/html.js
+++ node_scripts/html.js
@@ -1,12 +1,23 @@
 const fs = require('fs')
 const { render } = require('@riotjs/ssr')
 const register = require('@riotjs/ssr/register')
+const srcDirFromRoot = './src/html'
+const srcDirFromFile = '../src/html'
 
 register()
 
-const Root = require('../src/html/root.riot').default
-const html = render('html', Root)
+const generateHtml = (dir, file) => {
+  const Root = require(`${ dir }/${ file }`).default
+  const html = render('html', Root)
+  fs.writeFile('dist/root.html', html, (err) => {
+    if (err) console.log(err)
+  })
+  // console.log(html)
+}
 
-fs.writeFile('dist/root.html', html, (err) => {
-  if (err) console.log(err)
+const dirEntry = fs.readdirSync(srcDirFromRoot, { withFileTypes: true })
+const fileArray = dirEntry.filter(dirent => dirent.isFile()).map(({ name }) => name)
+
+fileArray.forEach(file => {
+  generateHtml(srcDirFromFile, file)
 })
