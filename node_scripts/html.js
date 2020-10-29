79e29eb5112d8826cb467a677b03cd6745b98098
Genki Katsutani <nibushibu@gmail.com>
2020-10-29 17:03:57 +0900

    HTML生成の元ディレクトリの参照方法を修正

--- node_scripts/html.js
+++ node_scripts/html.js
@@ -6,26 +6,26 @@ const srcDirFromRoot = './src/html'
 const srcDirFromFile = '../src/html'
 const outpurtDir = 'dist'
 
-console.log(__dirname)
-console.log(path.basename(__dirname))
+const pages = JSON.parse(fs.readFileSync(path.join(srcDirFromRoot, 'pages.json')))
 
 register()
 
 const generateHtml = (dir, file) => {
-  const Root = require(`${ dir }/${ file }`).default
+  const Root = require(`${dir}/${file}`).default
   const html = render('html', Root)
   const outputFile = file.replace(/riot$/, 'html')
   fs.writeFile(`${outpurtDir}/${outputFile}`, html, (err) => {
     if (err) console.log(err)
   })
-  // console.log(html)
 }
 
 /**
  * ディレクトリ内の自動的に読み込んで出力
  */
 const dirEntry = fs.readdirSync(srcDirFromRoot, { withFileTypes: true })
-const fileArray = dirEntry.filter(dirent => dirent.isFile()).map(({ name }) => name)
-fileArray.forEach(file => {
-  generateHtml(srcDirFromFile, file)
+const fileArray = dirEntry
+  .filter((dirent) => dirent.isFile())
+  .map(({ name }) => name)
+fileArray.forEach((file) => {
+  if (file.match(/\.riot$/)) generateHtml(srcDirFromFile, file)
 })
