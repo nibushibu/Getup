877fa28a6f425a6bdbbf95a6ee63a95620f5c54c
Genki Katsutani <nibushibu@gmail.com>
2020-10-29 10:00:06 +0900

    子コンポーネントも静的に生成

--- node_scripts/html.js
+++ node_scripts/html.js
@@ -3,21 +3,25 @@ const { render } = require('@riotjs/ssr')
 const register = require('@riotjs/ssr/register')
 const srcDirFromRoot = './src/html'
 const srcDirFromFile = '../src/html'
+const outpurtDir = 'dist'
 
 register()
 
 const generateHtml = (dir, file) => {
   const Root = require(`${ dir }/${ file }`).default
   const html = render('html', Root)
-  fs.writeFile('dist/root.html', html, (err) => {
+  const outputFile = file.replace(/riot$/, 'html')
+  fs.writeFile(`${outpurtDir}/${outputFile}`, html, (err) => {
     if (err) console.log(err)
   })
   // console.log(html)
 }
 
+/**
+ * ディレクトリ内の自動的に読み込んで出力
+ */
 const dirEntry = fs.readdirSync(srcDirFromRoot, { withFileTypes: true })
 const fileArray = dirEntry.filter(dirent => dirent.isFile()).map(({ name }) => name)
-
 fileArray.forEach(file => {
   generateHtml(srcDirFromFile, file)
 })
