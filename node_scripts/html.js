2927f442e7592b9ecf09452ccfdc23a6d34f8032
Genki Katsutani <nibushibu@gmail.com>
2020-11-21 01:33:25 +0900

    WIP

--- node_scripts/html.js
+++ node_scripts/html.js
@@ -7,23 +7,24 @@ const srcDirFromRoot = './src/html'
 const srcDirFromFile = '../src/html'
 const outputDir = 'dist'
 const file = path.join(srcDirFromFile, 'html.riot')
-const pages = JSON.parse(fs.readFileSync(path.join(srcDirFromRoot, 'pages.json')))
-const getDirName = require('path').dirname
-
+const pages = JSON.parse(
+  fs.readFileSync(path.join(srcDirFromRoot, 'pages.json'))
+)
 
 // Riot コンポーネントを require できるように
 register()
 
-const generateHtml = (outputPath) => {
+const generateHtml = (outputPath, meta) => {
   const Root = require(file).default
-  const html = render('html', Root)
+  const html = render('html', Root, { meta: meta })
   const fileName = outputPath.match(/\.html?$/) ? '' : 'index.html'
-  mkdirp.sync(path.parse(path.join(outputDir, outputPath)).dir)
-  fs.writeFile(path.join(outputDir, outputPath, fileName), html, err => {
-    if (err) throw err
+  mkdirp(path.parse(path.join(outputDir, outputPath)).dir).then(() => {
+    fs.writeFile(path.join(outputDir, outputPath, fileName), html, (err) => {
+      if (err) throw err
+    })
   })
 }
 
-pages.forEach(page => {
-  generateHtml(page.path)
+pages.forEach((page) => {
+  generateHtml(page.path, page.meta)
 })
