5960617f31b913fa2b8e74142d4b20f8cb838548
Genki Katsutani <nibushibu@gmail.com>
2020-12-03 03:34:19 +0900

    WIP

--- node_scripts/html.js
+++ node_scripts/html.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const glob = require('glob')
 const mkdirp = require('mkdirp')
 const { render } = require('@riotjs/ssr')
 const register = require('@riotjs/ssr/register')
@@ -11,8 +12,18 @@ const file = path.join(srcDirFromFile, 'index.riot')
 // Riot コンポーネントを require できるように
 register()
 
-const Root = require(file).default
-const html = render('html', Root)
-fs.writeFile('./dist/index.html', html, (err) => {
-  if (err) throw err
+glob(`${srcDirFromRoot}/**/*.riot`, (err, files) => {
+  if (err) return err
+  generateHtml(files)
 })
+
+const generateHtml = (files) => {
+  files.forEach(file => {
+    const Root = require(`.${file}`).default
+    const html = render('html', Root)
+    const dir = path.join(outputDir, file.replace(srcDirFromRoot, '').replace(/riot$/, 'html') )
+    fs.writeFile(dir, html, (err) => {
+      if (err) throw err
+    })
+  })
+}
