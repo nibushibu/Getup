fda86a1a154227dab7767328326ddd2e16c62c9d
Genki Katsutani <nibushibu@gmail.com>
2020-12-03 00:42:21 +0900

    JSONを使わずにriotファイルからHTMLを生成する形に修正

--- node_scripts/html.js
+++ node_scripts/html.js
@@ -3,28 +3,16 @@ const path = require('path')
 const mkdirp = require('mkdirp')
 const { render } = require('@riotjs/ssr')
 const register = require('@riotjs/ssr/register')
-const srcDirFromRoot = './src/html'
-const srcDirFromFile = '../src/html'
+const srcDirFromRoot = './src/html/pages'
+const srcDirFromFile = '../src/html/pages'
 const outputDir = 'dist'
-const file = path.join(srcDirFromFile, 'html.riot')
-const pages = JSON.parse(
-  fs.readFileSync(path.join(srcDirFromRoot, 'pages.json'))
-)
+const file = path.join(srcDirFromFile, 'index.riot')
 
 // Riot コンポーネントを require できるように
 register()
 
-const generateHtml = (outputPath, title, meta) => {
-  const Root = require(file).default
-  const html = render('html', Root, { title: title, meta: meta })
-  const fileName = outputPath.match(/\.html?$/) ? '' : 'index.html'
-  mkdirp(path.parse(path.join(outputDir, outputPath)).dir).then(() => {
-    fs.writeFile(path.join(outputDir, outputPath, fileName), html, (err) => {
-      if (err) throw err
-    })
-  })
-}
-
-pages.forEach((page) => {
-  generateHtml(page.path, page.title, page.meta, )
+const Root = require(file).default
+const html = render('html', Root)
+fs.writeFile('./dist/index.html', html, (err) => {
+  if (err) throw err
 })
