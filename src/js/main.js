bc5a4f14cf134741f77793c3f0af18ee637305d5
Genki Katsutani <genki@nibushibu.com>
2021-05-05 08:46:24 +0900

    不要な main.js を削除

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/stable';
-import '@webcomponents/template';
-import { register, mount } from 'riot';
-import App from './components/my-app.js';
-register('my-app', App);
-import RawHtml from './components/raw-html.js';
-register('raw-html', RawHtml);
-mount('[data-riot]');
