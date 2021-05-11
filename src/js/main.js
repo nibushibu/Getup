79b11203775994836346b3391608f9f1e8f0470d
Genki Katsutani <genki@nibushibu.com>
2021-05-11 09:49:10 +0900

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,10 @@ import 'core-js/stable'
 import '@webcomponents/template'
 import { register, mount, install } from 'riot'
 
-import App from './components/my-app.js'
+import App from './components/src/riot/my-app.js'
 register('my-app', App)
 
-import RawHtml from './components/raw-html.js'
+import RawHtml from './components/src/riot/raw-html.js'
 register('raw-html', RawHtml)
 
 mount('[data-riot]')
