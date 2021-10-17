663f6c69782785020fbd6d66b14f7f143a33b982
Genki Katsutani <genki@nibushibu.com>
2021-10-17 14:51:47 +0900

    main.js の記述を整理

--- src/js/main.js
+++ src/js/main.js
@@ -8,23 +8,24 @@ import RawHtml from './components/src/riot/raw-html.js'
 register('my-app', App)
 register('raw-html', RawHtml)
 
-install((component) => {
-  /**
-   * GETパラメーターをObjectとして返す関数
-   * @return {object}
-   */
-  component.getObjectFromLocationSearch = () => {
-    const paramsString = window.location.search.replace('?', '')
-    if (!paramsString) return {}
-    const paramsArray = paramsString.split('&')
-    const resultObject = {}
-    paramsArray.forEach((param) => {
-      const array = param.split('=')
-      resultObject[array[0]] = array[1]
-    })
-    return resultObject
-  }
+/**
+ * GETパラメーターをObjectとして返す関数
+ * @return {object}
+ */
+function getObjectFromLocationSearch() {
+  const paramsString = window.location.search.replace('?', '')
+  if (!paramsString) return {}
+  const paramsArray = paramsString.split('&')
+  const resultObject = {}
+  paramsArray.forEach((param) => {
+    const array = param.split('=')
+    resultObject[array[0]] = array[1]
+  })
+  return resultObject
+}
 
+install((component) => {
+  component.getObjectFromLocationSearch = getObjectFromLocationSearch
   return component
 })
 
