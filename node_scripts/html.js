15f129a8d28da60a82fba86125eda7835a905474
Genki Katsutani <genki@interfeel.co.jp>
2023-03-09 15:23:13 +0900

    Updated: html.js
    support glob v9

--- node_scripts/html.js
+++ node_scripts/html.js
@@ -1,34 +1,26 @@
 const fs = require('fs')
 const path = require('path')
-const glob = require('glob')
+const { Glob } = require('glob')
 const mkdirp = require('mkdirp')
 const { render } = require('@riotjs/ssr')
 const register = require('@riotjs/register')
-const prettier = require('prettier')
-const srcDirFromRoot = './src/html/pages'
+const srcDirFromRoot = 'src/html/pages'
 const outputDir = 'dist'
 
 // Riot コンポーネントを require できるように
 register({ exts: ['.html'] })
 
-glob(`${srcDirFromRoot}/**/*.riot.html`, (err, files) => {
-  if (err) return err
-  generateHtml(files)
+const files = new Glob(`${srcDirFromRoot}/**/*.riot.html`, {
+  withFileTypes: true
 })
+for (const file of files) {
+  const Root = require(`../${srcDirFromRoot}/${file.name}`).default
+  const html = render('html', Root)
+  const dir = path.join(outputDir, file.name.replace(/riot\.html$/, 'html'))
 
-const generateHtml = (files) => {
-  files.forEach((file) => {
-    const Root = require(`.${file}`).default
-    const html = prettier.format(render('html', Root), { parser: 'html' })
-    const dir = path.join(
-      outputDir,
-      file.replace(srcDirFromRoot, '').replace(/riot\.html$/, 'html')
-    )
-
-    mkdirp(path.parse(dir).dir).then(() => {
-      fs.writeFile(dir, html, (err) => {
-        if (err) throw err
-      })
+  mkdirp(path.parse(dir).dir).then(() => {
+    fs.writeFile(dir, html, (err) => {
+      if (err) throw err
     })
   })
 }
