eb6efd74996d6cba635a206283eede4102336374
Genki Katsutani <genki@interfeel.co.jp>
2023-09-09 01:57:30 +0900

    WIP

--- node_scripts/html.js
+++ node_scripts/html.js
@@ -3,24 +3,31 @@ import path from 'path'
 import { Glob } from 'glob'
 import { mkdirp } from 'mkdirp'
 import render from '@riotjs/ssr'
-import register from '@riotjs/register'
 
 const srcDirFromRoot = 'src/html/pages'
 const outputDir = 'dist'
 
-register({ exts: ['.html'] })
-
-const files = new Glob(`${srcDirFromRoot}/**/*.riot.html`, {
-  withFileTypes: true
+const files = new Glob(`${srcDirFromRoot}/**/*.riot`, {
+  withFileTypes: false
 })
-for (const file of files) {
-  const Root = import(`../${srcDirFromRoot}/${file.name}`)
-  const html = render('html', Root)
-  const dir = path.join(outputDir, file.name.replace(/riot\.html$/, 'html'))
 
-  mkdirp(path.parse(dir).dir).then(() => {
-    fs.writeFile(dir, html, (err) => {
-      if (err) throw err
+for await (const file of files) {
+  console.log(file)
+  import(`../${file}`).then((riotHTML) => {
+    // console.log(riotHTML)
+    const html = render('html', riotHTML)
+    console.log(html)
+    const dir = path.join(
+      outputDir,
+      file
+        .replace(new RegExp(`${srcDirFromRoot}/`), '')
+        .replace(/riot$/, 'html')
+    )
+    console.log(dir)
+    mkdirp(path.parse(dir).dir).then(() => {
+      fs.writeFile(dir, html, (err) => {
+        if (err) throw err
+      })
     })
   })
 }
