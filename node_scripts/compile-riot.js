21497a84bf9333ff85cfaf959eed99f633b838fd
Genki Katsutani <genki@interfeel.co.jp>
2023-09-09 03:50:32 +0900

    不要なファイルを削除

--- node_scripts/compile-riot.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-const { Glob } = require('glob')
-const { mkdirp } = require('mkdirp')
-import { compile } from '@riotjs/compiler'
-
-console.log(process.argv)
